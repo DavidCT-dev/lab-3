7185352d3cdb4f37636d1759c84b1b092ba2825b
David_Caba <ct55609@gmail.com>
2022-09-28 19:23:30 -0400

    lab3

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Theme } from './theme/Theme'
+//import Palette from './components/palette/index'
+import ViewPort from './components/viewport/index'
+function App() {
+  return (
+    <div className="App">
+      <Theme>
+        {//My THEME
+} 
+        <ViewPort/>
+        {//<Palette/>
+}
+      </Theme>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/viewport/viewport.css.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles (theme =>({
+    wrapper:{
+        justifyContent: 'center',
+        color:'red',
+        fontSize:'40px',
+        textAlign:'center'
+    },
+    main: {
+        fontSize:'50px',
+    }
+}));
